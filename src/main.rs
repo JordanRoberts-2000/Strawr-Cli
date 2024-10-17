10edb469e683110d2c29154a2160301f52553e24
Jordan Roberts <jordanroberts333@icloud.com>
2024-10-17 08:09:15 +0100

    feat: dotenv added and setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
+    dotenv().ok();
+    let api_key = env::var("OPENAI_API_KEY").expect("API key not found in .env file");
+    println!("{api_key}");
     println!("Hello, world!");
 }
