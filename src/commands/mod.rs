aea43ea393cb6a9a942c89d5bb357de0e6dccb07
Jordan Roberts <jordanroberts333@icloud.com>
2025-01-24 18:52:38 +0000

    chor: added descriptions to commands

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -5,16 +5,26 @@ pub mod playground;
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
-    #[command(arg_required_else_help(true))]
-    // Img(ImgCommands),
-    Grab,
-    Template,
+    #[command(about = "Create or manipulate additions for your current projects")]
     Add,
-    Font,
-    Snippets,
-    Project,
-    Playground(PlaygroundCommands),
-    Uninstall,
-    Config,
+    #[command(about = "Backup saved data")]
     Backup,
+    #[command(about = "Open or reset configuration file")]
+    Config,
+    #[command(about = "Download google fonts")]
+    Font,
+    // #[command(arg_required_else_help(true), about = "Modify image files")]
+    // Img(ImgCommands),
+    #[command(about = "Open saved data folder")]
+    Open,
+    #[command(about = "Use playground environment")]
+    Playground(PlaygroundCommands),
+    #[command(about = "Set and get encrypted data")]
+    Grab,
+    #[command(about = "Create or manipulate code snippets")]
+    Snippets,
+    #[command(about = "Create or manipulate templates for your projects")]
+    Template,
+    #[command(about = "Uninstalls cli tool and deletes its saved data")]
+    Uninstall,
 }
--- src/commands/playground/mod.rs
+++ src/commands/playground/mod.rs
@@ -1,10 +1,12 @@
 use args::PlaygroundCommands;
 use dirs;
 
+use crate::state::AppContext;
+
 pub mod args;
 
 impl PlaygroundCommands {
-    pub fn handle_command(&self) {
+    pub fn handle_command(&self, ctx: &AppContext) {
         println!("Hello form playground");
         if let Some(home_dir) = dirs::home_dir() {
             println!("Home directory: {}", home_dir.display());
