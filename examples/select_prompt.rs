d6a4bc5d17ad62a27b5b17bc0e6eadce86560c73
Jordan Roberts <jordanroberts333@icloud.com>
2025-05-17 23:32:34 +0100

    feat(template): updated select to include generic Display type

--- examples/select_prompt.rs
+++ examples/select_prompt.rs
@@ -1,17 +1,27 @@
 use strawr::prompt::{traits::SelectPrompt, UserInput};
 
+#[derive(Debug)]
+pub enum Fruit {
+    Apple,
+    Banana,
+}
+impl std::fmt::Display for Fruit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
 fn main() {
-    let fruits: Vec<String> = vec![
+    let fruits = vec![
         "apple",
         "peach",
         "pears",
         "plums",
         "bananas",
         "strawberries",
-    ]
-    .iter()
-    .map(|s| s.to_string())
-    .collect();
+    ];
+
+    let fruits_enum = vec![Fruit::Apple, Fruit::Banana];
 
     let input = UserInput;
     match input.select(&fruits, "Choose a fruit:\n") {
