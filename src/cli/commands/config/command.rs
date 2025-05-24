87188971b574d97e6623188763a97f9ae9df2976
Jordan Roberts <jordanroberts333@icloud.com>
2025-05-24 18:42:37 +0100

    feat(config): added --open flag for opening config file shortcut

--- src/cli/commands/config/command.rs
+++ src/cli/commands/config/command.rs
@@ -8,11 +8,25 @@ use super::{enums::ConfigOption, ConfigCommandError};
 pub struct ConfigCommand {
     #[arg(short, long, ignore_case = true)]
     pub editor: Option<Editor>,
+    #[arg(
+      short = 'o',
+      long = "open",
+      action = clap::ArgAction::SetTrue,
+      help = "Open the configuration file and exit"
+    )]
+    pub open: bool,
 }
 
 impl ConfigCommand {
     pub fn execute(&self, ctx: &CliContext) -> Result<(), ConfigCommandError> {
         let editor = self.editor.as_ref().unwrap_or(&ctx.config.default_editor);
+
+        if self.open {
+            ctx.service
+                .init_launch_editor()
+                .launch_editor(editor, &ctx.config_path)?;
+            return Ok(());
+        }
 
         let msg = "Select option:";
         let options: Vec<ConfigOption> = ConfigOption::iter().collect();
